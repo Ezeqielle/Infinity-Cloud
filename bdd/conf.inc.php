5bb1e905be431ce8ae07f16d51096b5b9229a999
MrWolf <35033692+Ezeqielle@users.noreply.github.com>
2020-02-04 10:37:59 +0100

    Update conf.inc.php

--- bdd/conf.inc.php
+++ bdd/conf.inc.php
@@ -1,15 +1,1 @@
-<?php
 
-define('DBHOST', 'localhost');
-define('DBDRIVER', 'mysql');
-define('DBNAME', 'infinity_cloud');
-define('DBUSER', 'infinity');
-define('DBPWD', 'cloud93');
-
-/*
-define('DBHOST', 'localhost');
-define('DBDRIVER', 'mysql');
-define('DBNAME', 'infinity_cloud');
-define('DBUSER', 'root');
-define('DBPWD', '');
-*/
